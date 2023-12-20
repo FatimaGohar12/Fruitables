031b88479071d3afc61a976b281d6679148d7153
FatimaGohar12 <fatimagohar296@gmail.com>
2023-12-20 15:42:00 +0500

    Responsiveness issues

--- src/Components/Testimonal.jsx
+++ src/Components/Testimonal.jsx
@@ -5,21 +5,21 @@ import React from "react";
 
 const testimonalData = [
   {
-    clientImage: "/img/testimonial-1.jpg", // Corrected file extension to '.jpg'
+    clientImage: "/img/testimonial-1.jpg",
     clientName: "Jenny",
     clientProfession: "Doctor",
     clientReview:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Voluptatum aut esse vel. Sunt aliquid aperiam ullam dolor est",
   },
   {
-    clientImage: "/img/testimonial-1.jpg", // Corrected file extension to '.jpg'
+    clientImage: "/img/testimonial-1.jpg", 
     clientName: "Jenny",
     clientProfession: "Doctor",
     clientReview:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Voluptatum aut esse vel. Sunt aliquid aperiam ullam dolor est",
   },
   {
-    clientImage: "/img/testimonial-1.jpg", // Corrected file extension to '.jpg'
+    clientImage: "/img/testimonial-1.jpg", 
     clientName: "Jenny",
     clientProfession: "Engineer",
     clientReview:
@@ -56,95 +56,87 @@ const testimonalData = [
 ];
 
 const Testimonal = () => {
- 
   return (
     <>
       <Flex
         // border="1px solid black"
-        width="90%"
-        height="500px"
+        width="auto"
+        height="400px"
         margin="auto"
         alignItems="center"
-      > <Splide
-      options={{
-        perPage: 3,
-        arrows: false,
-        wheel: true,
-        // height: '10rem',
-        rewind: true,
-        // gap: '1rem',
-        // breakpoints: {
-        //   420: {
-        //     perPage: 1,
-        //   },
-        //   768: {
-        //     perPage: 2,
-        //   },
-        // },
-      }}
-      aria-label="My Favorite Images"
-      onMoved={(splide, newIndex) => {
-        // eslint-disable-next-line
-        console.log("moved", newIndex);
+      >
+      
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            wheel: true,
+            // height: '10rem',
+            rewind: true,
+           
+          }}
+          aria-label="My Favorite Images"
+          onMoved={(splide, newIndex) => {
+            // eslint-disable-next-line
+            console.log("moved", newIndex);
 
-        // eslint-disable-next-line
-        console.log("length", splide.length);
-      }}
-    >
-        {testimonalData.map((item, index) => (
-           <SplideSlide key={index}>
-
-          <Flex
-          cursor="pointer"
-            key={index} // Add a unique key for each mapped element
-            border="1px solid white"
-            width="88%"
-            height="270px"
-            padding="12px"
-            borderRadius="12px"
-            alignItems="center"
-            justifyContent="space-around"
-            flexDir="column"
-            bgColor="#f4f6f8"
-            margin="10px" // Add margin for spacing between elements
-          >
-            <Flex width="100%" height="120px" textAlign="center">
-              <Text fontSize="1rem">{item.clientReview}</Text>
-            </Flex>
-
-            <Box
-              width="100%"
-              height="2px"
-              bg="#FFA500" // Orange color
-              my="10px" // Margin to separate the line from the text
-            />
-            <Flex
-              width="100%"
-              height="150px"
-              alignItems="center"
-              flexDir="row"
-              justifyContent="center"
-            >
+            // eslint-disable-next-line
+            console.log("length", splide.length);
+          }}
+        >
+          {testimonalData.map((item, index) => (
+            <SplideSlide key={index}>
               <Flex
-                width="20%"
-                height="100px"
-                bg={`url('${item.clientImage}') center / cover no-repeat`} // Use backticks for dynamic URL
+                cursor="pointer"
+                key={index}
+                border="1px solid white"
+                width="88%"
+                height="270px"
+                padding="12px"
                 borderRadius="12px"
-              ></Flex>
-              <Flex
                 alignItems="center"
                 justifyContent="space-around"
                 flexDir="column"
-                width="50%"
-                height="100px"
+                bgColor="#f4f6f8"
+                margin="10px" 
               >
-                <Text fontSize="2rem">{item.clientName}</Text>
-                <Text fontSize="1rem">{item.clientProfession}</Text>
+                <Flex width="100%" height="120px" textAlign="center">
+                  <Text fontSize="1rem">{item.clientReview}</Text>
+                </Flex>
+
+                <Box
+                  width="100%"
+                  height="2px"
+                  bg="#FFA500" 
+                  my="10px" 
+                />
+                <Flex
+                  width="100%"
+                  height="150px"
+                  alignItems="center"
+                  flexDir="row"
+                  justifyContent="center"
+                >
+                  <Flex
+                    width="20%"
+                    height="100px"
+                    bg={`url('${item.clientImage}') center / cover no-repeat`} // Use backticks for dynamic URL
+                    borderRadius="12px"
+                  ></Flex>
+                  <Flex
+                    alignItems="center"
+                    justifyContent="space-around"
+                    flexDir="column"
+                    width="50%"
+                    height="100px"
+                  >
+                    <Text fontSize="2rem">{item.clientName}</Text>
+                    <Text fontSize="1rem">{item.clientProfession}</Text>
+                  </Flex>
+                </Flex>
               </Flex>
-            </Flex>
-          </Flex>
-           </SplideSlide>
-        ))}
+            </SplideSlide>
+          ))}
         </Splide>
       </Flex>
     </>
--- src/TestimonalsHeading.jsx
+++ src/TestimonalsHeading.jsx
@@ -7,7 +7,7 @@ const TestimonalsHeading = () => {
       <Flex
         // border="1px solid black"
         width="100%"
-        height="300px"
+        height="200px"
         alignItems="center"
         justifyContent="center"
         flexDir="column"
